741d8d4831724091936be88d8c79cb7ccaa6bb85
James Correa <james.correa9090@gmail.com>
2021-11-05 07:02:43 -1000

    commit tree 37a835d072d12114b3f98238681a2b3aba7c0b3d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(array, string) {
+    const matchingList = array.filter((driver) => (driver.toUpperCase() === string.toUpperCase()));
+
+    return matchingList
+}
+
+function fuzzyMatch(array, name) {
+    const match = array.filter((comp) => (comp.charAt() === name.charAt()));
+    return match;
+}
+
+// function matchName(array, name) {
+//     const match = array.filter((driver) => (driver.length() === name.length()))
+//     return match
+// }
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
